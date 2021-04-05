a23e19022947af78dd6266007882e2f671702841
lindama <lindima200@gmail.com>
2021-04-04 23:34:28 -0600

    Add social links to user profiles

--- lib/home/Profile.dart
+++ lib/home/Profile.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Profile extends StatelessWidget {
   final DocumentSnapshot document;
 
   Profile({Key key, @required this.document}) : super(key: key);
+
+  void _launchSocial(String url, String fallbackUrl) async {
+  // Don't use canLaunch because of fbProtocolUrl (fb://)
+  try {
+    bool launched =
+        await launch(url, forceSafariVC: false, forceWebView: false);
+    if (!launched) {
+      await launch(fallbackUrl, forceSafariVC: false, forceWebView: false);
+      }
+  } catch (e) {
+    await launch(fallbackUrl, forceSafariVC: false, forceWebView: false);
+  }
+}
 
   @override
   Widget build(BuildContext context) {
@@ -103,6 +116,32 @@ class Profile extends StatelessWidget {
                 Text(
                   document.data()['hobbies'],
                 ),
+                Padding(
+                  padding: EdgeInsets.all(15.0),
+                ),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      //replace with urls from firestore 
+                      GestureDetector(
+                        onTap: () {_launchSocial('fb://profile/408834569303957', 'https://www.facebook.com/dorockxl');
+                        },
+                        child: 
+                        Container(child: Image.asset("assets/images/fb.png", scale: 7)
+                      )),
+                      GestureDetector(
+                        onTap: () {_launchSocial('https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=lindama200@gmail.com', '');
+                        },
+                        child: 
+                        Container(child: Image.asset("assets/images/email.png", scale: 7)
+                      )),
+                      GestureDetector(
+                        onTap: () {_launchSocial('https://www.linkedin.com/in/linda--ma/', '');
+                        },
+                        child: 
+                        Container(child: Image.asset("assets/images/linkedin.png", scale: 7)
+                      )),
+                    ]),
               ]),
         ))]),
       ),
