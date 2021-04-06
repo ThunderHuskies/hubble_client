0bbe6eab142d87dac3396535d4ef433d8e03a1f6
Martin Au-yeung <you@example.com>
2021-04-05 18:31:17 -0700

    make icons a teeny bit smaller

--- lib/home/Profile.dart
+++ lib/home/Profile.dart
@@ -9,17 +9,17 @@ class Profile extends StatelessWidget {
   Profile({Key key, @required this.document}) : super(key: key);
 
   void _launchSocial(String url, String fallbackUrl) async {
-  // Don't use canLaunch because of fbProtocolUrl (fb://)
-  try {
-    bool launched =
-        await launch(url, forceSafariVC: false, forceWebView: false);
-    if (!launched) {
-      await launch(fallbackUrl, forceSafariVC: false, forceWebView: false);
+    // Don't use canLaunch because of fbProtocolUrl (fb://)
+    try {
+      bool launched =
+          await launch(url, forceSafariVC: false, forceWebView: false);
+      if (!launched) {
+        await launch(fallbackUrl, forceSafariVC: false, forceWebView: false);
       }
-  } catch (e) {
-    await launch(fallbackUrl, forceSafariVC: false, forceWebView: false);
+    } catch (e) {
+      await launch(fallbackUrl, forceSafariVC: false, forceWebView: false);
+    }
   }
-}
 
   @override
   Widget build(BuildContext context) {
@@ -27,137 +27,146 @@ class Profile extends StatelessWidget {
       body: Stack(children: <Widget>[
         SingleChildScrollView(
           child: Column(children: [
-         Container(
-          alignment: Alignment.center,
-          child: 
-          FittedBox(
-            child: Image(
-              image: NetworkImage(document.data()['image']),
-              height: (MediaQuery.of(context).size.height)/2,
-              width: MediaQuery.of(context).size.width,
-              fit: BoxFit.fitWidth
-          )),
+            Container(
+              alignment: Alignment.center,
+              child: FittedBox(
+                  child: Image(
+                      image: NetworkImage(document.data()['image']),
+                      height: (MediaQuery.of(context).size.height) / 2,
+                      width: MediaQuery.of(context).size.width,
+                      fit: BoxFit.fitWidth)),
+            ),
+            Padding(
+              padding: EdgeInsets.all(15.0),
+            ),
+            ConstrainedBox(
+                constraints: BoxConstraints(maxWidth: 350),
+                child: Container(
+                  alignment: Alignment.topLeft,
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          document.data()['name'],
+                          style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(3.0),
+                        ),
+                        Text("University of British Columbia",
+                            style: TextStyle(fontSize: 17)),
+                        Text("Sophomore", style: TextStyle(fontSize: 17)),
+                        Padding(
+                          padding: EdgeInsets.all(5.0),
+                        ),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                  height: 30,
+                                  width: 100,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                      color: Colors.black,
+                                    ),
+                                    borderRadius: BorderRadius.circular(10.0),
+                                  ),
+                                  child: Center(
+                                      child:
+                                          Text(document.data()['courses'][0]))),
+                              Container(
+                                  height: 30,
+                                  width: 100,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                      color: Colors.black,
+                                    ),
+                                    borderRadius: BorderRadius.circular(10.0),
+                                  ),
+                                  child: Center(
+                                      child:
+                                          Text(document.data()['courses'][1]))),
+                              Container(
+                                  height: 30,
+                                  width: 100,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                      color: Colors.black,
+                                    ),
+                                    borderRadius: BorderRadius.circular(10.0),
+                                  ),
+                                  child: Center(
+                                      child:
+                                          Text(document.data()['courses'][2]))),
+                            ]),
+                        Padding(
+                          padding: EdgeInsets.all(5.0),
+                        ),
+                        Divider(
+                          color: Colors.black,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(5.0),
+                        ),
+                        Text(
+                          document.data()['hobbies'],
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(15.0),
+                        ),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              //replace with urls from firestore
+                              GestureDetector(
+                                  onTap: () {
+                                    _launchSocial(
+                                        'fb://profile/408834569303957',
+                                        'https://www.facebook.com/dorockxl');
+                                  },
+                                  child: Container(
+                                      child: Image.asset("assets/images/fb.png",
+                                          scale: 10))),
+                              GestureDetector(
+                                  onTap: () {
+                                    _launchSocial(
+                                        'https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=lindama200@gmail.com',
+                                        '');
+                                  },
+                                  child: Container(
+                                      child: Image.asset(
+                                          "assets/images/email.png",
+                                          scale: 10))),
+                              GestureDetector(
+                                  onTap: () {
+                                    _launchSocial(
+                                        'https://www.linkedin.com/in/linda--ma/',
+                                        '');
+                                  },
+                                  child: Container(
+                                      child: Image.asset(
+                                          "assets/images/linkedin.png",
+                                          scale: 10))),
+                            ]),
+                      ]),
+                ))
+          ]),
         ),
-        Padding(
-          padding: EdgeInsets.all(15.0),
+        new Positioned(
+          top: 0.0,
+          left: 10.0,
+          right: 10.0,
+          child: AppBar(
+              leading: new IconButton(
+                icon: new Icon(Icons.arrow_back_ios, color: Colors.black),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+              backgroundColor: Colors.transparent,
+              elevation: 0.0),
         ),
-        ConstrainedBox(
-          constraints: BoxConstraints(maxWidth: 350),
-          child: Container(
-          alignment: Alignment.topLeft,
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  document.data()['name'],
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(3.0),
-                ),
-                Text(
-                  "University of British Columbia",
-                   style: TextStyle(fontSize: 17)
-                ),
-                Text(
-                  "Sophomore",
-                   style: TextStyle(fontSize: 17)
-                ),
-                Padding(
-                  padding: EdgeInsets.all(5.0),
-                ),
-                Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        height: 30,
-                        width: 100,
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.black,
-                          ),
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                        child: Center(child: Text(document.data()['courses'][0]))
-                      ),
-                      Container(
-                        height: 30,
-                        width: 100,
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.black,
-                          ),
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                        child: Center(child: Text(document.data()['courses'][1]))
-                      ),
-                      Container(
-                        height: 30,
-                        width: 100,
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.black,
-                          ),
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                        child: Center(child: Text(document.data()['courses'][2]))
-                      ),
-                    ]),
-                    Padding(
-                      padding: EdgeInsets.all(5.0),
-                    ),
-                Divider(
-                  color: Colors.black,
-                ),
-                Padding(
-                  padding: EdgeInsets.all(5.0),
-                ),
-                Text(
-                  document.data()['hobbies'],
-                ),
-                Padding(
-                  padding: EdgeInsets.all(15.0),
-                ),
-                Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      //replace with urls from firestore 
-                      GestureDetector(
-                        onTap: () {_launchSocial('fb://profile/408834569303957', 'https://www.facebook.com/dorockxl');
-                        },
-                        child: 
-                        Container(child: Image.asset("assets/images/fb.png", scale: 7)
-                      )),
-                      GestureDetector(
-                        onTap: () {_launchSocial('https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=lindama200@gmail.com', '');
-                        },
-                        child: 
-                        Container(child: Image.asset("assets/images/email.png", scale: 7)
-                      )),
-                      GestureDetector(
-                        onTap: () {_launchSocial('https://www.linkedin.com/in/linda--ma/', '');
-                        },
-                        child: 
-                        Container(child: Image.asset("assets/images/linkedin.png", scale: 7)
-                      )),
-                    ]),
-              ]),
-        ))]),
-      ),
-      new Positioned(
-                  top: 0.0,
-                  left: 10.0,
-                  right: 10.0,
-                  child: AppBar(
-                    leading: new IconButton(
-                      icon: new Icon(Icons.arrow_back_ios, color: Colors.black),
-                      onPressed: () => Navigator.of(context).pop(),
-                    ),
-                    backgroundColor: Colors.transparent, 
-                    elevation: 0.0
-                  ),),
-              ]),
-      );
+      ]),
+    );
   }
 }
