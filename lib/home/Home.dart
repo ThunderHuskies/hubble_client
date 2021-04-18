f1409509faea89a576ca75e3945e32774f070bb0
lindama <lindima200@gmail.com>
2021-04-18 16:36:49 -0600

    Adds functionality to remove user from connections list

--- lib/home/Home.dart
+++ lib/home/Home.dart
@@ -458,22 +458,74 @@ class _ConnectionsState extends State<Connections> {
                                                 ),
                                               ));
                                         },
-                                        child: Column(children: <Widget>[
-                                          ListTile(
-                                            leading: CircleAvatar(
-                                              backgroundImage:
-                                                  CachedNetworkImageProvider(
-                                                      document
-                                                          .data()!['image']),
-                                            ),
-                                            title:
-                                                Text(document.data()!['name']),
-                                            subtitle:
-                                                Text(document.data()!['major']),
-                                            trailing:
-                                                Icon(Icons.message_outlined),
-                                          )
-                                        ])));
+                                        child: Dismissible(
+                                            background: Container(
+                                              color: Colors.red,
+                                              child: Icon(
+                                                Icons.delete,
+                                                color: Colors.white,
+                                              )
+                                              ),
+                                            key: Key(widget.user!.uid),
+                                            confirmDismiss: (DismissDirection direction) async {
+                                              await showDialog(
+                                                context:
+                                                    context,
+                                                builder:
+                                                    (BuildContext
+                                                        context) {
+                                                  return CupertinoAlertDialog(
+                                                      title: new Text(
+                                                          "‼️"),
+                                                      content:
+                                                          new Text(
+                                                              "Are you sure you want to remove ${document.data()!['name']} as a connection?"),
+                                                      actions: <Widget>[
+                                                        new FlatButton(
+                                                            onPressed: () =>
+                                                              setState(() => {
+                                                                Navigator.of(context).pop(true),
+                                                                //removes user from your connections list
+                                                                FirebaseFirestore.instance.collection('users').doc(widget.user!.uid).update({
+                                                                  'connections': FieldValue.arrayRemove([document.id])
+                                                                })
+                                                                .then((value) => print('Deleted ${document.id} from connections'))
+                                                                .catchError((error) => print('Error with deleting user: ${document.id} => $error')),
+                                                                //removes you from the user's connections list
+                                                                FirebaseFirestore.instance.collection('users').doc(document.id).update({
+                                                                  'connections': FieldValue.arrayRemove([widget.user!.uid])
+                                                                })
+                                                                .then((value) => print('Deleted you from ${document.id} connections'))
+                                                                .catchError((error) => print('Error with deleting user: ${widget.user!.uid} => $error')),
+                                                                ScaffoldMessenger
+                                                                  .of(context)
+                                                                  .showSnackBar(SnackBar(content: Text('${document.data()!['name']} removed as connection'))),
+                                                              }),
+                                                            child: Text("Remove", style: TextStyle(color: Colors.blue[800])
+                                                           )),
+                                                        new FlatButton(
+                                                          onPressed: () => Navigator.of(context).pop(false),
+                                                          child:  Text("Cancel", style: TextStyle(color: Colors.blue[800]))
+                                                        ),
+                                                      ]);
+                                                });
+                                            },
+                                            child: Column(children: <Widget>[
+                                            ListTile(
+                                              leading: CircleAvatar(
+                                                backgroundImage:
+                                                    CachedNetworkImageProvider(
+                                                        document
+                                                            .data()!['image']),
+                                              ),
+                                              title:
+                                                  Text(document.data()!['name']),
+                                              subtitle:
+                                                  Text(document.data()!['major']),
+                                              trailing:
+                                                  Icon(Icons.message_outlined),
+                                            )
+                                ]))));
                               }).toList(),
                             );
                         }
--- lib/registration/user.dart
+++
@@ -1,8 +0,0 @@
-// class User {
-//   final String id;
-//   final String firstName;
-//   final String lastName;
-//   final String phoneNumber;
-// }
-
-// User({this.id, this.firstName})
